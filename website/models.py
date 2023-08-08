f26b6fd9bf1a43b80c9ccb0ac65c24e1f63e7ba1
mdomeracki231994 <domerackimark@gmail.com>
2023-08-08 10:31:47 -0400

    Adding contact info table

--- website/models.py
+++ website/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactInfo(models.Model):
+    name = models.CharField(max_length=155)
+    email = models.EmailField()
+    message = models.TextField()
--- website/views.py
+++ website/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ContactInfo
 
 
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        ContactInfo.objects.create(
+            name=name,
+            email=email,
+            message=message
+        )
+        return redirect('home')
     return render(request, 'website/index.html')
+
+
+
