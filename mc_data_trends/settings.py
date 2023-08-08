dede920b01542c7a236c24a8084c97808c96020d
mdomeracki231994 <domerackimark@gmail.com>
2023-08-08 17:03:52 -0400

    Adding debug info

--- mc_data_trends/settings.py
+++ mc_data_trends/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 if DEBUG_STRING == 'True':
-    DEBUG = False
+    DEBUG = True
 
 ALLOWED_HOSTS = []
 
