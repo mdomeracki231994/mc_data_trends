5b372030e67364f3966d77c0a9ef3c2a5e29c43d
mdomeracki231994 <domerackimark@gmail.com>
2023-08-14 10:41:41 -0400

    Adding more pages

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'website'
+
 urlpatterns = [
     path('', views.index, name='home'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+    path('thank-you/', views.thankyou_page, name='thank-you'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -4,19 +4,24 @@ from django.contrib import messages
 
 
 def index(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-
-        ContactInfo.objects.create(
-            name=name,
-            email=email,
-            message=message
-        )
-        messages.success(request, 'Thank you for reaching out, we will be in touch!')
-        return redirect('home')
     return render(request, 'website/index.html')
 
 
+def services(request):
+    return render(request, 'website/services.html')
 
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        print(name, email, subject, message)
+        return redirect('website:thank-you')
+
+    return render(request, 'website/contact.html')
+
+
+def thankyou_page(request):
+    return render(request, 'website/thank-you.html')
