9d7f4eb1a7521f7571fcfb28701fd216436ef098
mdomeracki231994 <domerackimark@gmail.com>
2023-08-22 17:52:45 -0400

    Adding returns to models

--- survey/models.py
+++ survey/models.py
@@ -11,17 +11,29 @@ class PersonInfo(models.Model):
     phone_number = models.CharField(max_length=50)
     email = models.EmailField()
 
+    def __str__(self):
+        return f'Business: {self.business_name}, Contact: {self.name}'
+
 
 class Question(models.Model):
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     question = models.CharField(max_length=255)
+
+    def __str__(self):
+        self.question
 
 
 class Response(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     person = models.ForeignKey(PersonInfo, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.person.name}'
+
 
 class Answer(models.Model):
     response = models.ForeignKey(Response, on_delete=models.CASCADE, related_name='answers')
     answer = models.TextField()
+
+    def __str__(self):
+        return f'{self.response.person.name}, {self.response.person.business_name}, {self.answer}'
